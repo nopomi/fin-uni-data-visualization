cf3b787804ec4362a2891985a375848058250b81
Miska Noponen <miska.noponen@gmail.com>
2020-04-20 19:40:52 +0300

    Re-created project from Dash template.

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Finuni - an app for comparing Finnish Universities funding data.',
+    author='Miska Noponen',
+    license='MIT',
+)
